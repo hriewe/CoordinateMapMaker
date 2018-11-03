e4d6b171b31ee6b577d63da81b5e7e4f185f244c
Hayden Riewe <hriewe12@gmail.com>
2018-11-03 00:00:34 -0400

    Added user input

--- map.py
+++ map.py
@@ -1,9 +1,18 @@
+# Hayden Riewe
+# CoordinateMapMaker
+# v 0.1
+# hrcyber.tech
+# www.github.com/hriewe
+
 import os
-from urllib.request import pathname2url # Python 3.x
+from urllib.request import pathname2url
 import folium
 import webbrowser
 
-map = folium.Map(location = [40.897934, -73.885948], zoom_start = 7)
+longitude = float(input("Enter the lognitude: "))
+latitude = float(input("Enter the latitude: "))
+
+map = folium.Map(location = [longitude, latitude], zoom_start = 7)
 print(map.save('map.html'))
 
 url = 'file:{}'.format(pathname2url(os.path.abspath('map.html')))
